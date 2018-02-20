6e5db9e7e7b58c5432cea5780213997269555aaf
陈垂耿 <chuigengchen@gmail.com>
2018-02-20 16:20:04 +0800

    注入 this 对象

--- listeners/demo.js
+++ listeners/demo.js
@@ -3,7 +3,7 @@ const Listener = require('../lib/Listener');
 class DemoListener extends Listener {
 
   constructor(options) {
-    super(options.name, (options.cron || '* * * * * *'), function() {
+    super(options.name, (options.cron || '* * * * * *'), function(ctx) {
       console.log('hi! My name is demo!');
     });
   }
